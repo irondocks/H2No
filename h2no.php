9f6be2696e76d055121ed3bd8777ba3eb9ae924a
wise-penny <inland14@live.com>
2023-02-02 23:50:08 -0500

    Update h2no.php

--- h2no.php
+++ h2no.php
@@ -207,16 +207,5 @@
 			$this->db = unserialize($this->db);
 			return $this;
 		}
-	}
-
-	$g = serialize(json_decode(file_get_contents("six.json")));
-	file_put_contents("six.serialized",$g);	
-	$h2no = new H2No("six.serialized");
-	// $h2no->set_h2no("six.serialized");
-	$h2no->load_db("six.serialized");
-	$h2no->delete("ten");
-	$h2no->update(["eight" => ["seven" => 17]]);
-	var_dump($h2no->result);
-	$h2no->save("sixc.serialized");
-	
+	}	
 ?>
